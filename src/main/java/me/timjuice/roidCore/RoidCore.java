204ed10832720958c52c9619d2bb285a437c6970
tjuic20-foi <tim.juic@gmail.com>
2024-10-18 22:00:56 +0200

    first commit

---
+++ src/main/java/me/timjuice/roidCore/RoidCore.java
@@ -0,0 +1,17 @@
+package me.timjuice.roidCore;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RoidCore extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
