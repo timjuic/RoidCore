40947f81bfb215c7bd866cbe2ac06bc66831ee97
tjuic20-foi <tim.juic@gmail.com>
2024-10-26 16:53:35 +0200

    Add default values if they dont exist in message config

--- src/main/java/me/timjuice/roidCore/config/CoreMessageConfig.java
+++ src/main/java/me/timjuice/roidCore/config/CoreMessageConfig.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import me.timjuice.roidCore.RoidCore;
 import me.timjuice.roidCore.utils.ConsoleLogger;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
@@ -14,6 +15,7 @@ public class CoreMessageConfig {
     protected File configFile;
     protected FileConfiguration config;
 
+    private String messagePrefix;
     private String helpMessageHeader;
     private String helpCategoryHeader;
     private String noPermissionMessage;
@@ -33,28 +35,35 @@ public class CoreMessageConfig {
      */
     private void generateDefaultConfig() {
         configFile = new File(roidPlugin.getDataFolder(), "messages.yml");
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+
+        // Add default values
+        config.addDefault("message-prefix", "&f&lRoidCore &8» ");
+        config.addDefault("help-message-header", "&8&m--------------&8[ &a&l{PLUGIN_NAME} &8]&m--------------");
+        config.addDefault("help-category-header", "&8&m--------------&8[ &a&l{CATEGORY} &2&lHelp &8]&m--------------");
+        config.addDefault("no-permission-message", "&cYou don't have permission to use this!");
+        config.addDefault("only-players-command", "&cOnly players can use this command!");
+        config.addDefault("invalid-command-message", "&cThat command doesn't exist! Type /{PLUGIN_NAME} for help.");
+        config.addDefault("help-command-group-format", "&a/{BASE_CMD} help {CMD_GROUP} &f- &7Shows {CMD_GROUP} commands");
+        config.addDefault("help-individual-subcommand-format", "&a/{BASE_CMD} {SUB_CMD_NAME} &f- &7{SUB_CMD_DESCRIPTION}");
+
+        // If the file doesn't exist, save defaults and create the file
         if (!configFile.exists()) {
+            roidPlugin.getDataFolder().mkdirs();
             try {
-                // Create the directory if it doesn't exist
-                roidPlugin.getDataFolder().mkdirs();
-                // Create the default config file
-                configFile.createNewFile();
-
-                // Write default values to the config file
-                FileConfiguration config = roidPlugin.getConfig();
-                config.set("help-message-header", "&8&m--------------&8[ &a&l{PLUGIN_NAME} &8]&m--------------");
-                config.set("help-category-header", "&8&m--------------&8[ &a&l{CATEGORY} &2&lHelp &8]&m--------------");
-                config.set("no-permission-message", "&cYou don't have permission to use this!");
-                config.set("only-players-command", "&cOnly players can use this command!");
-                config.set("invalid-command-message", "&cThat command doesn't exist! Type /{PLUGIN_NAME} for help.");
-                config.set("help-command-group-format", "&a/{BASE_CMD} help {CMD_GROUP} &f- &7Shows {CMD_GROUP} commands");
-                config.set("help-individual-subcommand-format", "&a/{BASE_CMD} {SUB_CMD_NAME} &f- &7{SUB_CMD_DESCRIPTION}");
-
-                // Save the config
                 config.save(configFile);
                 ConsoleLogger.success(roidPlugin, "Default configuration created successfully.");
             } catch (IOException e) {
                 ConsoleLogger.error(roidPlugin, "Could not create default configuration: " + e.getMessage());
+            }
+        } else {
+            // If the file exists, just save any new defaults added
+            config.options().copyDefaults(true);
+            try {
+                config.save(configFile);
+                ConsoleLogger.success(roidPlugin, "Configuration updated with default values.");
+            } catch (IOException e) {
+                ConsoleLogger.error(roidPlugin, "Could not update configuration: " + e.getMessage());
             }
         }
     }
@@ -65,6 +74,7 @@ public class CoreMessageConfig {
     private void loadDefaultConfig() {
         ConfigLoader configLoader = new ConfigLoader(roidPlugin, new File(roidPlugin.getDataFolder(), "messages.yml"));
         // Load each configuration value with validation
+        messagePrefix = configLoader.getColoredString("message-prefix");
         helpMessageHeader = configLoader.getColoredString("help-message-header");
         helpCategoryHeader = configLoader.getColoredString("help-category-header");
         noPermissionMessage = configLoader.getColoredString("no-permission-message");
