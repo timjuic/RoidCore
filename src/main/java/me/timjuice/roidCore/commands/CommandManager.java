1343eef5d085485660232f3672287462a42ceb6d
tjuic20-foi <tim.juic@gmail.com>
2024-12-07 14:03:29 +0100

    Add missing event registration call

--- src/main/java/me/timjuice/roidCore/commands/CommandManager.java
+++ src/main/java/me/timjuice/roidCore/commands/CommandManager.java
@@ -56,6 +56,8 @@ public class CommandManager implements CommandExecutor, TabCompleter {
             roidPlugin.getCommand(baseCmdName).setExecutor(this);
             roidPlugin.getCommand(baseCmdName).setTabCompleter(this);
         }
+
+        registerPreprocessListener();
     }
 
     public CommandManager(RoidCore roidPlugin, String baseCmdName, String[] aliases) {
